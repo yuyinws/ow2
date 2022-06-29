6370b67f71f37a9b83d8ce97107137bfc4e2dc20
yuyinws <lyc657508366@gmail.com>
2022-06-29 20:45:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200" h-screen>
+    <router-view />
+  </main>
+</template>
+
+<style>
+main {
+  background-image: url('/owbg.jpg');
+}
+</style>
+
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { reactive, ref } from 'vue'
+</script>
+
+<template>
+  ow2
+</template>
+
+<style scoped lang="scss">
+
+</style>
